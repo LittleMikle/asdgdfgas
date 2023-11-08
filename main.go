cd1c31f46e707ccbc14fabd225f024e7d8784319
M <little.mikle@yahoo.com>
2023-11-08 13:34:47 +0300

    refactored

--- main.go
+++ main.go
@@ -2,26 +2,47 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"log"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strconv"
-	"strings"
 	"sync"
 )
 
+type Config struct {
+	PathDir  string
+	ReadFile string
+	ResDir   string
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
+
 func main() {
 	var files []string
-	var resA int
-	var resB int
-	var resC int
-	var resFF int
 
 	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
+	ch := make(chan map[string]int)
 
-	err := filepath.Walk("./testing", func(path string, info os.FileInfo, err error) error {
+	err := initConfig()
+	if err != nil {
+		log.Fatal().Msg("error with viper")
+	} else {
+		log.Info().Msg("Config initiation successful")
+	}
+	cfg := Config{
+		PathDir:  viper.GetString("pathDir"),
+		ReadFile: viper.GetString("readFile"),
+		ResDir:   viper.GetString("resDir"),
+	}
+
+	err = filepath.Walk(cfg.PathDir, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			name := info.Name()
 			files = append(files, name)
@@ -29,91 +50,46 @@ func main() {
 		return nil
 	})
 	if err != nil {
-		log.Fatalf("failed with open directory")
+		log.Fatal().Msg("failed with open directory")
 	}
 
-	chA := make(chan int, len(files))
-	chB := make(chan int, len(files))
-	chC := make(chan int, len(files))
-	chFF := make(chan int, len(files))
-
-	for _, file := range files {
+	go func() {
 		wg.Add(1)
-		go func(file string) {
-			defer wg.Done()
-			readFile, err := os.Open("./testing/" + file)
+		defer wg.Done()
+		resMap := map[string]int{}
+
+		for _, file := range files {
+			readFile, err := os.Open(cfg.ReadFile + file)
 			if err != nil {
-				fmt.Println(err)
+				log.Error().Err(err).Msg("failed with readfile")
 			}
 			fileScanner := bufio.NewScanner(readFile)
 
 			fileScanner.Split(bufio.ScanLines)
 
 			for fileScanner.Scan() {
-				if strings.Contains(fileScanner.Text(), "a") {
-					str := regexp.MustCompile(`\D+`).ReplaceAllString(fileScanner.Text(), "")
-					val, _ := strconv.Atoi(str)
-					chA <- val
-				}
-				if strings.Contains(fileScanner.Text(), "b") {
-					str := regexp.MustCompile(`\D+`).ReplaceAllString(fileScanner.Text(), "")
-					val, _ := strconv.Atoi(str)
-					chB <- val
-				}
-				if strings.Contains(fileScanner.Text(), "c") {
-					str := regexp.MustCompile(`\D+`).ReplaceAllString(fileScanner.Text(), "")
-					val, _ := strconv.Atoi(str)
-					chC <- val
-				}
-				if strings.Contains(fileScanner.Text(), "ff") {
-					str := regexp.MustCompile(`\D+`).ReplaceAllString(fileScanner.Text(), "")
-					val, _ := strconv.Atoi(str)
-					chFF <- val
-				}
+				key := regexp.MustCompile(`[^a-zA-Z]+`).ReplaceAllString(fileScanner.Text(), "")
+				valueStr := regexp.MustCompile(`\D+`).ReplaceAllString(fileScanner.Text(), "")
+				value, _ := strconv.Atoi(valueStr)
+				mu.Lock()
+				resMap[key] += value
+				mu.Unlock()
 			}
 			readFile.Close()
-		}(file)
-	}
+		}
+		ch <- resMap
+		close(ch)
+	}()
 	wg.Wait()
-	close(chA)
-	close(chB)
-	close(chC)
-	close(chFF)
 
-	for a := range chA {
-		resA += a
-	}
-	for b := range chB {
-		resB += b
-	}
-	for c := range chC {
-		resC += c
-	}
-	for ff := range chFF {
-		resFF += ff
-	}
+	resMap := <-ch
 
-	file, _ := os.Create("./testing/result.txt")
+	file, _ := os.Create(cfg.ResDir)
 	w := bufio.NewWriter(file)
-	lines := []string{"a:", "b:", "c:", "ff:"}
-	for _, s := range lines {
-		if s == "a:" {
-			w.WriteString(s + strconv.Itoa(resA) + "\n")
-		}
-		if s == "b:" {
-			w.WriteString(s + strconv.Itoa(resB) + "\n")
-		}
-		if s == "c:" {
-			w.WriteString(s + strconv.Itoa(resC) + "\n")
-		}
-		if s == "ff:" {
-			w.WriteString(s + strconv.Itoa(resFF) + "\n")
-		}
+
+	for k, v := range resMap {
+		w.WriteString(k + ":" + strconv.Itoa(v) + "\n")
 	}
 	w.Flush()
-
-	fmt.Printf("RESULT A = %d\n", resA)
-	fmt.Printf("RESULT B = %d\n", resB)
-	fmt.Printf("RESULT C = %d\n", resC)
-	fmt.Printf("RESULT FF = %d\n", resFF)
+	log.Info().Msg("File created successfully")
 }
